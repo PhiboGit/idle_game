1590d3c2876e6e081e29a5d1af1622aed54a2c7a
Phibo <phibo96@gmail.com>
2023-09-19 01:54:07 +0200

    weighted choices

--- src/game/utils/randomDice.js
+++ src/game/utils/randomDice.js
@@ -2,36 +2,79 @@ function rollDice(range){
     return Math.floor(Math.random() * range + 1)
 }
 
-
-function choice(events, size, probability) {
-    if(probability != null) {
-      const pSum = probability.reduce((sum, v) => sum + v);
-      if(pSum < 1 - Number.EPSILON || pSum > 1 + Number.EPSILON) {
-        throw Error("Overall probability has to be 1.");
-      }
-      if(probability.find((p) => p < 0) != undefined) {
-        throw Error("Probability can not contain negative values");
-      }
-      if(events.length != probability.length) {
-        throw Error("Events have to be same length as probability");
-      }
-    } else {
-      probability = new Array(events.length).fill(1/events.length);
+function weightedChoice(events, size, weights) {
+  if (weights != null) {
+    if (events.length !== weights.length) {
+      throw Error("Events and weights must have the same length.");
     }
 
-    var probabilityRanges = probability.reduce((ranges, v, i) => {
-      var start = i > 0 ? ranges[i-1][1] : 0 - Number.EPSILON;
-      ranges.push([start, v + start + Number.EPSILON]);
-      return ranges;
-    }, []);
-
-    var choices = new Array();
-    for(var i = 0; i < size; i++) {
-      var random = Math.random();
-      var rangeIndex = probabilityRanges.findIndex((v, i) => random > v[0] && random <= v[1]);
-      choices.push(events[rangeIndex]);
+    if (weights.some(weight => weight < 0)) {
+      throw Error("Weights cannot contain negative values.");
     }
-    return choices;
+  } else{
+    if (weights.length === 0) {
+      // If no weights are provided, assign equal weights
+      weights = new Array(events.length).fill(1);
+    }
+  }
+
+  const choices = []
+
+
+  for (let i = 0; i < size; i++) {
+    let totalWeight = 0;
+    const random = Math.random() * weights.reduce((a, b) => a + b, 0);
+
+    for (let j = 0; j < events.length; j++) {
+      totalWeight += weights[j];
+      if (random < totalWeight) {
+        choices.push(events[j]);
+        break;
+      }
+    }
+  }
+
+  return choices;
 }
 
-module.exports = {choice, rollDice}
+function weightedChoiceRemoved(events, size, weights) {
+  let eventsCopy = events.slice();
+  let weightsCopy = (weights != null) ? weights.slice() : new Array(events.length).fill(1);
+  if (eventsCopy.length !== weightsCopy.length) {
+    throw Error("Events and weights must have the same length.");
+  }
+
+  if (weightsCopy.some(weight => weight < 0)) {
+    throw Error("Weights cannot contain negative values.");
+  }
+
+  const choices = [];
+
+  // Limit the size to the number of available events
+  size = Math.min(size, eventsCopy.length);
+
+  for (let i = 0; i < size; i++) {
+    if (eventsCopy.length === 0) {
+      // If there are no more events to choose from, break out of the loop.
+      break;
+    }
+
+    let totalWeight = 0;
+    const random = Math.random() * weightsCopy.reduce((a, b) => a + b, 0);
+
+    for (let j = 0; j < eventsCopy.length; j++) {
+      totalWeight += weightsCopy[j];
+      if (random < totalWeight) {
+        choices.push(eventsCopy[j]);
+        // Remove the selected event and its weight from the copies
+        eventsCopy.splice(j, 1);
+        weightsCopy.splice(j, 1);
+        break;
+      }
+    }
+  }
+
+  return choices;
+}
+
+module.exports = {weightedChoice,weightedChoiceRemoved, rollDice}
