64c6fe7ad526f548dc61063b5641dff2405e59eb
Phibo <phibo96@gmail.com>
2023-11-28 01:04:02 +0100

    rollRange can now take floats

--- src/game/utils/randomDice.js
+++ src/game/utils/randomDice.js
@@ -3,7 +3,7 @@ function rollDice(sides){
 }
 
 function rollRange(min, max){
-  return Math.floor(Math.random() * (max - min + 1)) + min
+  return Math.floor((Math.random() * (max - min + 1)) + min)
 }
 
 function weightedChoice(events, size, weights) {
