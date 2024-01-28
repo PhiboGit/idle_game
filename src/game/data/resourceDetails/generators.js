e76074f2eba71fffe207508b6cf8837d2c68af3a
Phibo <phibo96@gmail.com>
2024-01-28 22:28:59 +0100

    new resourceItem generator

--- src/game/data/resourceDetails/generators.js
+++ src/game/data/resourceDetails/generators.js
@@ -1,7 +1,7 @@
 function generateEquipmentObject(name, displayName, description="", slot, equipmentSkills= [], tiers = [1,2,3,4,5], rarityList = ["common", "uncommon", "rare", "epic", "legendary"]) {
   const itemObject = {};
 
-  for (const tier in tiers) {
+  for (const tier of tiers) {
     for (const rarity of rarityList) {
       const key = `${name}T${tier}_${rarity}`;
       itemObject[key] = {
--- src/game/data/resourceDetails/resourcesInfo.js
+++ src/game/data/resourceDetails/resourcesInfo.js
@@ -4,15 +4,15 @@ const {craftingMaterials, charms, gatheredResources, refinedResources} = require
 
 
 const items = {
-  ...generateEquipmentObject("pickaxe", "Pickaxe", "Used for mining.", "tool", [1,2,3,4,5], ["common", "uncommon", "rare", "epic", "legendary"]),
-  ...generateEquipmentObject("sickle", "Sickle", "Used for harvesting.", "tool", [1,2,3,4,5], ["common", "uncommon", "rare", "epic", "legendary"]),
-  ...generateEquipmentObject("axe", "Axe", "Used for woodcutting.", "tool", [1,2,3,4,5], ["common", "uncommon", "rare", "epic", "legendary"]),
+  ...generateEquipmentObject("pickaxe", "Pickaxe", "Used for mining.", "tool", ["mining"], [1,2,3,4,5], ["common", "uncommon", "rare", "epic", "legendary"]),
+  ...generateEquipmentObject("sickle", "Sickle", "Used for harvesting.", "tool", ["harvesting"], [1,2,3,4,5], ["common", "uncommon", "rare", "epic", "legendary"]),
+  ...generateEquipmentObject("axe", "Axe", "Used for woodcutting.", "tool", ["woodcutting"], [1,2,3,4,5], ["common", "uncommon", "rare", "epic", "legendary"]),
 
-  ...generateEquipmentObject("hat", "Hat", "Keeps your head warm.", "hat", [1,2,3,4,5], ["common", "uncommon", "rare", "epic", "legendary"]),
-  ...generateEquipmentObject("chestpiece", "Chestplate", "", "chestpiece", [1,2,3,4,5], ["common", "uncommon", "rare", "epic", "legendary"]),
-  ...generateEquipmentObject("gloves", "Gloves", "", "gloves", [1,2,3,4,5], ["common", "uncommon", "rare", "epic", "legendary"]),
-  ...generateEquipmentObject("pants", "Pants", "", "pants", [1,2,3,4,5], ["common", "uncommon", "rare", "epic", "legendary"]),
-  ...generateEquipmentObject("boots", "Boots", "", "boots", [1,2,3,4,5], ["common", "uncommon", "rare", "epic", "legendary"]),
+  ...generateEquipmentObject("hat", "Hat", "Keeps your head warm.", "hat", ["mining", "woodcutting", "harvesting"], [1,2,3,4,5], ["common", "uncommon", "rare", "epic", "legendary"]),
+  ...generateEquipmentObject("chestpiece", "Chestplate", "", "chestpiece", ["mining", "woodcutting", "harvesting"] ,[1,2,3,4,5], ["common", "uncommon", "rare", "epic", "legendary"]),
+  ...generateEquipmentObject("gloves", "Gloves", "", "gloves", ["mining", "woodcutting", "harvesting"], [1,2,3,4,5], ["common", "uncommon", "rare", "epic", "legendary"]),
+  ...generateEquipmentObject("pants", "Pants", "", "pants", ["mining", "woodcutting", "harvesting"], [1,2,3,4,5], ["common", "uncommon", "rare", "epic", "legendary"]),
+  ...generateEquipmentObject("boots", "Boots", "", "boots", ["mining", "woodcutting", "harvesting"], [1,2,3,4,5], ["common", "uncommon", "rare", "epic", "legendary"]),
   
 }
 
