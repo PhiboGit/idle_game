e215688bbe399ccebba021496b0846d388aa3610
Phibo <phibo96@gmail.com>
2024-02-04 23:52:49 +0100

    loot table comments for OR and AND table

--- src/game/data/lootTables.js
+++ src/game/data/lootTables.js
@@ -1,6 +1,14 @@
 const {rollDice, rollRange, adjustWeights, weightedChoice} = require('../utils/randomDice')
 const {lootTables} = require('../utils/dataLoader')
 
+/**
+ * This is an AND table. 
+ * you get each entry that is within the range of the rolled random number
+ * 
+ * @param {*} table 
+ * @param {*} luck player luck
+ * @returns [{"item": x.item, "amount": rollRange(x.min, x.max)}]
+ */
 function rollTable(table, luck){
 
   roll = rollDice(table.maxRoll) + (table.luck ? luck : 0)
@@ -16,7 +24,14 @@ function rollTable(table, luck){
   return re
 }
 
-
+/**
+ * If size is 1 this is an OR table.
+ * 
+ * @param {*} table
+ * @param {*} size How many times to open this table. Returns number of size items
+ * @param {*} luck the players luck
+ * @returns [{"item": x.item, "amount": rollRange(x.min, x.max)}]
+ */
 function weightTable(table, size, luck){
   
   
@@ -34,6 +49,7 @@ function weightTable(table, size, luck){
   if (limitByLuck && startLimit && endLimit) {
     let index = 0
     for (let i = 0; i < limitByLuck.length; i++) {
+      // check which limits to use based on player luck
       if (luck >= limitByLuck[i]){
         index = i
       } else {
@@ -66,33 +82,45 @@ function parseLootTable(tableName, size= 1, luck = 0) {
   const results = [];
 
   // Recursive function to parse loot tables
+  // loot is either another table or the loot to collect
   function parseTable(table, size, luck) {
+    // two types of loot tables
+    // TYPE: roll. AND
     if (table.type === "roll") {
+      // open the table and get the loot
       const rolls = rollTable(table, luck);
       for (const roll of rolls) {
+        // if it is another table, start recursion
         if (roll.item.startsWith("[LTID]")) {
           const subTable = lootTables[roll.item.substring(6)];
           if (subTable) {
             parseTable(subTable, roll.amount, luck);
           }
+          // if not a table, just add the loot
         } else {
           results.push({ item: roll.item, amount: roll.amount });
         }
       }
+      // TYPE: weights. OR
     } else if (table.type === "weights") {
+      // open the table and get the loot
       const rolls = weightTable(table, size, luck);
       for (const roll of rolls) {
+        // if it is another table, start recursion
         if (roll.item.startsWith("[LTID]")) {
           const subTable = lootTables[roll.item.substring(6)];
           if (subTable) {
             parseTable(subTable, roll.amount, luck);
           }
+          // if not a table, just add the loot
         } else {
           results.push({ item: roll.item, amount: roll.amount });
         }
       }
     }
   }
+
+  // table is in the defined lootTable json
   const table = lootTables[tableName]
   if (!table) {
     throw new Error(`Couldn't find table: ${tableName}`)
